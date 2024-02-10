ae4505188f39e9ff254598feb403fc9aa49a51fe
krishnaiitd <kprasad.iitd@gmail.com>
2024-02-10 23:34:06 +0900

    added sbt dependency and build Apache Hudi table on 0.14.1 on local machine, at apache-hudi-examples/csv2hudi/src/main/scala/Main.scala

--- apache-hudi-examples/csv2hudi/build.sbt
+++ apache-hudi-examples/csv2hudi/build.sbt
@@ -10,21 +10,20 @@ val HudiVersion = "0.14.1"
 
 val GlueVersion = "4.0.0"
 
-lazy val root= (project in file("."))
+lazy val root = (project in file("."))
   .settings(
-    resolvers += "aws-glue-etl-artifacts" at "https://aws-glue-etl-artifacts.s3.amazonaws.com/release/",
+//    resolvers += "aws-glue-etl-artifacts" at "https://aws-glue-etl-artifacts.s3.amazonaws.com/release/",
     libraryDependencies ++= Seq(
 //      Apache Hudi dependency
-      "org.apache.hudi" %% "hudi-spark-bundle" % HudiVersion,
-//      AWS Hudi Dependency
-      "org.apache.hudi" % "hudi-aws-bundle" % HudiVersion % Provided,
-      "com.amazonaws" % "AWSGlueETL" % GlueVersion % Provided,
-
-//      Spark Dependency
-      "org.apache.spark" %% "spark-avro" % SparkVersion,
-      "org.apache.spark" %% "spark-core" % SparkVersion,
+      "org.apache.hudi" %% "hudi-spark3.3-bundle" % HudiVersion,
       "org.apache.spark" %% "spark-sql" % SparkVersion,
+      //      AWS Hudi Dependency
+      //      "org.apache.hudi" % "hudi-aws-bundle" % HudiVersion % Provided,
+      //      "com.amazonaws" % "AWSGlueETL" % GlueVersion % Provided,
+      //      Spark Dependency
+      //      "org.apache.spark" %% "spark-avro" % SparkVersion,
+      //      "org.apache.spark" %% "spark-core" % SparkVersion,
 //      Test Dependency
-      "org.scalatest" %% "scalatest" % "3.2.15" % "test"
+//      "org.scalatest" %% "scalatest" % "3.2.15" % "test"
     )
   )
---
+++ apache-hudi-examples/csv2hudi/src/main/scala/local/Main.scala
@@ -0,0 +1,80 @@
+package local
+
+import org.apache.spark.sql.SparkSession
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+    // @todo: Need to pass this in the sparkSession config, not one by one
+    val sparkConf = Map(
+      "spark.serializer" -> "org.apache.spark.serializer.KryoSerializer",
+      "spark.sql.catalog.spark_catalog" -> "org.apache.spark.sql.hudi.catalog.HoodieCatalog",
+      "spark.sql.extensions" -> "org.apache.spark.sql.hudi.HoodieSparkSessionExtension",
+      "spark.kryo.registrator" -> "org.apache.spark.HoodieSparkKryoRegistrar"
+    )
+
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[*]")
+      .appName("TestingSparkOnlyLocally")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.hudi.catalog.HoodieCatalog")
+      .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension")
+      .config("spark.kryo.registrator", "org.apache.spark.HoodieSparkKryoRegistrar")
+      .getOrCreate()
+
+    import org.apache.spark.sql.functions._
+    import spark.sqlContext.implicits._
+    val data = Seq(("1", "Prasad", 32), ("2", "Singh", 3), ("3", "Kumar", 76), ("4", "Prasad", 13))
+    val schema = Seq("id", "last_name", "age")
+
+    val rdd = spark.sparkContext.parallelize(data)
+
+    val df = rdd.toDF(schema:_*)
+
+    val partitionLabel = "_partition"
+//    add partitin column "_partition", only two partition as of now, i.e % 2 on age
+    val df2 = df.withColumn("_partition", when(col("age") % 2 === 0, "even").otherwise("old"))
+    print("Getting the data from partitioned data frame")
+    df2.printSchema()
+    df2.show()
+
+
+    val tableName = "partition_demo_hudi_cow"
+    val localBasePath = "file:///Users/krishna/Downloads/hudidb"
+//    val basePathCoW = "s3://<BUCKET_NAME>/bronze/medium-post-examples/apache-hudi/partition_demo_hudi_cow"
+    val basePathCoW = localBasePath + "/medium-post-examples/apache-hudi/cow_partition_demo_hudi"
+    val hudi_options = Map(
+      "hoodie.table.name" -> tableName,
+      "hoodie.datasource.write.recordkey.field" -> "id",
+      "hoodie.datasource.write.partitionpath.field" -> partitionLabel,
+      "hoodie.datasource.write.table.name" -> tableName,
+      "hoodie.datasource.write.operation" -> "upsert",
+      "hoodie.datasource.write.precombine.field" -> "id",
+      "hoodie.upsert.shuffle.parallelism" -> "2",
+      "hoodie.insert.shuffle.parallelism" -> "2",
+      "hoodie.datasource.write.storage.type" -> "COPY_ON_WRITE",
+    )
+
+    df2.write
+      .format("hudi")
+      .options(hudi_options)
+      .mode("append")
+      .save(basePathCoW)
+
+    println("Written to " + basePathCoW)
+   
+//    val basePathMoR = "s3://<BUCKET_NAME>/bronze/medium-post-examples/apache-hudi/partition_demo_hudi_mor"
+    val basePathMoR = localBasePath + "/medium-post-examples/apache-hudi/mor_partition_demo_hudi"
+
+    val morHudiOption = hudi_options.updated("hoodie.datasource.write.storage.type", "MERGE_ON_READ")
+
+    println("BasePathMoR: " + basePathMoR)
+    println("morHudiOption: " + morHudiOption.toString())
+
+    df2.write
+      .format("hudi")
+      .options(morHudiOption)
+      .mode("append")
+      .save(basePathMoR)
+  }
+}
